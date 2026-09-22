edbc5133c009dbebbecd481812d29bccbd9e5e7f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-21 20:29:36 -0400

    chore: match Ringside deployment flow (#7)

--- deployment/forge.sh
+++ deployment/forge.sh
@@ -9,9 +9,3 @@ cd "$FORGE_SITE_PATH"
     --no-interaction \
     --prefer-dist \
     --optimize-autoloader
-
-# Forge deploys into a release directory that already contains the tracked
-# public output. Clear that release-local output so Jigsaw can rebuild it.
-find public -mindepth 1 -maxdepth 1 -exec rm -rf -- {} +
-
-"$FORGE_PHP" vendor/bin/jigsaw build production --no-interaction
