3d9ce0d810d2f5dd839cba099ae97ec82d5b6892
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-21 20:08:34 -0400

    feat: migrate marketing site to Jigsaw (#5)

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Container\Container;
+use TightenCo\Jigsaw\Events\EventBus;
+use TightenCo\Jigsaw\Jigsaw;
+
+/** @var Container $container */
+/** @var EventBus $events */
+
+/*
+ * You can run custom code at different stages of the build process by
+ * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
+ */
